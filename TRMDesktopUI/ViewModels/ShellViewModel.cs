373ef2d3ab83b2e5037f9abef56b9f24e888cb44
bboti199 <b.boti199@yahoo.com>
2020-06-09 17:15:18 +0300

    Implemented Login functionality

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,17 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using TRMDesktopUI.EventModels;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEventModel>
     {
-        private LoginViewModel _loginVM;
+        private SalesViewModel _salesVM;
+        private IEventAggregator _events;
+        private SimpleContainer _container;
 
-        public ShellViewModel(LoginViewModel loginVM)
+        public ShellViewModel(IEventAggregator events,
+            SalesViewModel salesVM, SimpleContainer container)
         {
-            _loginVM = loginVM;
-            ActivateItem(_loginVM);
+            _events = events;
+
+            _salesVM = salesVM;
+            _container = container;
+
+            _events.Subscribe(this);
+
+            ActivateItem(_container.GetInstance<LoginViewModel>());
+        }
+
+        public void Handle(LogOnEventModel message)
+        {
+            ActivateItem(_salesVM);
         }
     }
 }
