2a77152f9e123c1ee83a1c218380223d560308b9
bboti199 <b.boti199@yahoo.com>
2020-06-20 20:35:34 +0300

    Finished before upgrade

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
+using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
@@ -17,6 +19,82 @@ namespace TRMDataManager.Controllers
             UserData data = new UserData();
 
             return data.GetUserById(userId).First();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/user/Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var users = userManager.Users.ToList();
+                var roles = context.Roles.ToList();
+
+                foreach (var user in users)
+                {
+                    ApplicationUserModel u = new ApplicationUserModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email
+                    };
+
+                    foreach (var role in user.Roles)
+                    {
+                        u.Roles.Add(role.RoleId, roles.Where(x => x.Id == role.RoleId).First().Name);
+                    }
+
+                    output.Add(u);
+                }
+            }
+
+            return output;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/user/Admin/GetAllRoles")]
+        public Dictionary<string, string> GetallRoles()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roles = context.Roles.ToDictionary(x => x.Id, x => x.Name);
+
+                return roles;
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/user/Admin/AddRole")]
+        public void AddRole(UserRolePairModel data)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(data.UserId, data.RoleName);
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/user/Admin/RemoveRole")]
+        public void RemoveRole(UserRolePairModel data)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(data.UserId, data.RoleName);
+            }
         }
     }
 }
--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -45,6 +45,11 @@ namespace TRMDesktopUI.ViewModels
             TryClose();
         }
 
+        public void UserManagement()
+        {
+            ActivateItem(IoC.Get<UserDisplayViewModel>());
+        }
+
         public void LogOut()
         {
             _loggedInUserModel.ResetUserModel();
